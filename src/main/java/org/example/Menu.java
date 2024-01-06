dc756edac564a928679230b76dc1238775d20f77
milad <baranimilad70@yahoo.com>
2024-01-06 22:19:08 +0330

    #4-adding publicMenu

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,5 +1,18 @@
 package org.example;
 
-public class Menu {
+import java.util.Scanner;
 
+public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+    public void publicMenu(){
+        System.out.println("***welcome***");
+        System.out.println("1 - sign up");
+        System.out.println("2 - log in");
+        System.out.println("3 - exit");
+        System.out.print("--->");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
 }
