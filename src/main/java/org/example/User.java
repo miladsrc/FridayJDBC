8ff23a3148ca98efaf977a56217dda0dc79fd389
milad <baranimilad70@yahoo.com>
2024-01-06 21:52:26 +0330

    #3-adding fields of user class in it

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,60 @@
 package org.example;
 
 public class User {
+    private String firstName;
+    private String astName;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String firstName, String astName, String username, String password) {
+        this.firstName = firstName;
+        this.astName = astName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getAstName() {
+        return astName;
+    }
+
+    public void setAstName(String astName) {
+        this.astName = astName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", astName='" + astName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
